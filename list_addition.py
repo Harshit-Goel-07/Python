3c4ac7366cc825579c111addae9519229774a4ab
Harshit Goel <149133231+Harshit-Goel-07@users.noreply.github.com>
2024-02-23 23:30:32 +0530

    Add files via upload

---
+++ list_addition.py
@@ -0,0 +1,13 @@
+list=['harshit', 'goel']
+list2=[500125668]
+list3=list+list2
+print(list3)
+
+if 'hi' in list3:
+    print("YES")
+else:
+    print("NO")
+if 'harshit' in list3:
+    print("harshit")
+else:
+    print("NO")
---
+++ p1.py
@@ -0,0 +1,20 @@
+print("\\")
+
+print(" ")
+print(" ")
+
+l1=[1,2,"harshit"]
+print(l1)
+print(l1[0])
+print(l1[2])
+print(" ")
+l1.append(7)
+print(l1)
+
+print(" ")
+print(" ")
+
+t1=(1,2,"harshit")
+print(t1)
+print(t1[0])
+print(t1[2])

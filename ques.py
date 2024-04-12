db9d1e8863176b742e5c945ffaf0e785180e74ea
Harshit Goel <149133231+Harshit-Goel-07@users.noreply.github.com>
2024-04-12 19:16:19 +0530

    Add files via upload

--- ques.py
+++ ques.py
@@ -1,12 +1,33 @@
-'''01/02/24
-wap to find the largest element in the list 
-'''
+# WAP to create the following dataset using numpy and pandas
+# sapid Fname Lname Subject Teacher Marks
+# use matplotlib/seaborn to draw the visualization plots
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-list=[10,30,20,40,15]
-largest_number = list[0]
-for num in list:
-    if num > largest_number:
-        largest_number = num
-print(largest_number)
+l1=["Hey", 123, "HI007"]
+n1=np.array([l1])
+print(n1)
+d=pd.DataFrame(data=np.arange(0,70).reshape(10,7))
+print(d)
+data = {
+    "sapid": [500125668, 500123847, 500123668],
+    "Fname": ["Harshit", "Sandeep", "Anshul"],
+    "Lname": ["Goel", "Negi", "Goel"],
+    "Subject": ["C", "Python", "Physics"],
+    "Teacher": ["Bhawana", "Sajid", "Kailash"],
+    "Marks": [97, 99, 81]
+}
 
+df=pd.DataFrame(data, index=[1,2,3])
+
+print(df)
+
+# Visualization using Matplotlib
+plt.figure(figsize=(8, 6))
+plt.bar(df['Fname'], df['Marks'], color='skyblue')
+plt.xlabel('Name')
+plt.ylabel('Marks')
+plt.title('Student Marks')
+plt.show()
